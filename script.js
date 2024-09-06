abc828b900c339be0c2ce72be03db4517b272a07
NirakChoun <nirakchoun@gmail.com>
2024-09-06 18:16:00 -0500

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(firstNum, secondNum, operator) {
+    let result;
+    switch(operator) {
+        case "+":
+            result = add(firstNum, secondNum);
+            return result;
+        case "-":
+            result = subtract(firstNum, secondNum);
+            return result;
+        case "*":
+            result = multiply(firstNum, secondNum);
+            return result;
+        case "/":
+            result = divide(firstNum, secondNum);
+            return result;
+    }
+}
