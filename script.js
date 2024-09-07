5c9eb81b82034f66cb64efad6685d7bcb0241c59
NirakChoun <nirakchoun@gmail.com>
2024-09-06 20:29:18 -0500

    Add displayNumber function

--- script.js
+++ script.js
@@ -31,3 +31,32 @@ function operate(firstNum, secondNum, operator) {
             return result;
     }
 }
+
+function displayNumber() {
+    const buttons = document.querySelectorAll(".row");
+    console.log(buttons[0].children.length);
+
+    const display = document.querySelector(".display");
+    let defaultNumber = 0;
+    display.textContent = defaultNumber;
+    let displayNumber = "";
+    let number = 0;
+    for (let i = 0; i < buttons.length; i++) {
+        let button = buttons[i].children;
+        for (let j = 0; j < button.length; j++) {
+            button[j].addEventListener("click", (e) => {
+                let clickBtn = e.target.value;
+                if (clickBtn >= 0 && clickBtn <= 9) {
+                    displayNumber += clickBtn;
+                    display.textContent = displayNumber;
+                    number = Number(displayNumber);
+                    console.log(number);
+                }
+            });
+        }
+    }
+}
+
+displayNumber();
+
+
