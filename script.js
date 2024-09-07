6e1ef37da2e66311dd0b3d1a515d3ae89445e4c7
NirakChoun <nirakchoun@gmail.com>
2024-09-07 18:08:00 -0500

    Modify calculator logic

--- script.js
+++ script.js
@@ -19,44 +19,99 @@ function operate(firstNum, secondNum, operator) {
     switch(operator) {
         case "+":
             result = add(firstNum, secondNum);
-            return result;
+            break;
         case "-":
             result = subtract(firstNum, secondNum);
-            return result;
+            break;
         case "*":
             result = multiply(firstNum, secondNum);
-            return result;
+            break;
         case "/":
             result = divide(firstNum, secondNum);
-            return result;
+            break;
     }
+    return result;
 }
 
-function displayNumber() {
-    const buttons = document.querySelectorAll(".row");
-    console.log(buttons[0].children.length);
+function displayNumber(element, content) {
+    element.textContent = content;
+}
 
-    const display = document.querySelector(".display");
-    let defaultNumber = 0;
-    display.textContent = defaultNumber;
-    let displayNumber = "";
-    let number = 0;
+function resetBtn() {
+    const buttons = document.querySelectorAll(".row");
     for (let i = 0; i < buttons.length; i++) {
         let button = buttons[i].children;
         for (let j = 0; j < button.length; j++) {
-            button[j].addEventListener("click", (e) => {
-                let clickBtn = e.target.value;
-                if (clickBtn >= 0 && clickBtn <= 9) {
-                    displayNumber += clickBtn;
-                    display.textContent = displayNumber;
-                    number = Number(displayNumber);
-                    console.log(number);
-                }
-            });
+            button[j].style.opacity = 1;
         }
     }
 }
 
-displayNumber();
+function resetCalculator() {
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = "";
+    displayFirstNumber = "";
+    displaySecondNumber = "";
+    displayNumber(display, firstNumber);
+    resetBtn();
+}
 
+const buttons = document.querySelectorAll(".row");
+const display = document.querySelector(".display");
+let defaultNumber = 0;
+displayNumber(display, defaultNumber);
+let displayFirstNumber = "";
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
+let displaySecondNumber = "";
+let onFirstNumber = true;
 
+for (let i = 0; i < buttons.length; i++) {
+    let button = buttons[i].children;
+    for (let j = 0; j < button.length; j++) {
+        button[j].addEventListener("click", (e) => {
+            let clickBtn = e.target.value;
+            if (clickBtn >= 0 && clickBtn <= 9 && onFirstNumber) {
+                displayFirstNumber += clickBtn;
+                displayNumber(display, displayFirstNumber);
+                firstNumber = Number(displayFirstNumber);
+                console.log(firstNumber);
+            }
+            
+            if (clickBtn === "+" || clickBtn === "-" || clickBtn === "*" || clickBtn === "/") {
+                resetBtn();
+                e.target.style.opacity = 0.8;
+                operator = clickBtn;
+                console.log(operator);
+                onFirstNumber = false;
+            }
+
+            if ((clickBtn >= 0 && clickBtn <= 9) && !onFirstNumber) {
+                displaySecondNumber += clickBtn;
+                displayNumber(display, displaySecondNumber);
+                secondNumber = Number(displaySecondNumber);
+                console.log(secondNumber);
+            }
+
+            if (clickBtn === "=") {
+                let result = operate(firstNumber, secondNumber, operator);
+                displayNumber(display, result);
+                console.log(result);
+                firstNumber = 0;
+                secondNumber = 0;
+                displayFirstNumber = "";
+                displaySecondNumber = "";
+                operator = "";
+                onFirstNumber = true;
+                resetBtn();
+            }
+
+            if (clickBtn === "clear") {
+                resetCalculator();
+            }
+
+        });
+    }
+}
